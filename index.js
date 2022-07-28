ec8a515a078c00deaf8f2e56b31562b050448231
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-07-28 17:47:20 -0300

    fix linter error

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const burger = document.querySelector('#hamburger');
-	const menu = document.querySelector('#menu');
-	const cross = document.querySelector('#cross');
-	//toggle menu function
-	function openMenu () {
+  const burger = document.querySelector('#hamburger');
+  const menu = document.querySelector('#menu');
+  const cross = document.querySelector('#cross');
+  // toggle menu function
+  function openMenu() {
     menu.style.display = 'block';
-	}
+  }
 
-	function closeMenu () {
+  function closeMenu() {
     menu.style.display = 'none';
-	}
+  }
 
-	burger.addEventListener('click', openMenu);
+  burger.addEventListener('click', openMenu);
 
-	cross.addEventListener('click', closeMenu);
-})
+  cross.addEventListener('click', closeMenu);
+});
--- speakers.js
+++ speakers.js
@@ -1,50 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const speakersList = [
-        {
+  const speakersList = [
+    {
       name: 'Sanzica Lepons Tiago',
-        pic: 'images/Picture3.png',
-        titles: 'Head of Department',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      },
-      {
-        name: 'Zicale Spon Agoitnas',
-        pic: 'images/Picture3.png',
-        titles: 'Business Manager',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      },
-      {
-        name: 'Santiago Pons Lezica',
-        pic: 'images/Picture3.png',
-        titles: 'Mathematics teacher',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      },
-      {
-        name: 'Snop Acizel Ogaitnas',
-        pic: 'images/Picture3.png',
-        titles: 'Philosopher',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      },
-      {
-        name: 'Santiago Pons Lezica',
-        pic: 'images/Picture3.png',
-        titles: 'Artist',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      },
-      {
-        name: 'Lezons Poica Tiagosan',
-        pic: 'images/Picture3.png',
-        titles: 'World Discoverer',
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      }];
-  
-      const speakerArea = document.querySelector('#section-speakers');
-      const seeMoreButton = document.querySelector('#showMoreButton');
-      const speakerCard = document.querySelectorAll('.speaker-card');
-  
-      function addSpeakers () {
-      speakerArea.innerHTML = '';
-      speakersList.forEach((speaker) => {
-        speakerArea.innerHTML += `
+      pic: 'images/Picture3.png',
+      titles: 'Head of Department',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      name: 'Zicale Spon Agoitnas',
+      pic: 'images/Picture3.png',
+      titles: 'Business Manager',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      name: 'Santiago Pons Lezica',
+      pic: 'images/Picture3.png',
+      titles: 'Mathematics teacher',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      name: 'Snop Acizel Ogaitnas',
+      pic: 'images/Picture3.png',
+      titles: 'Philosopher',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      name: 'Santiago Pons Lezica',
+      pic: 'images/Picture3.png',
+      titles: 'Artist',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      name: 'Lezons Poica Tiagosan',
+      pic: 'images/Picture3.png',
+      titles: 'World Discoverer',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    }];
+
+  const speakerArea = document.querySelector('#section-speakers');
+  const seeMoreButton = document.querySelector('#showMoreButton');
+
+  function addSpeakers() {
+    speakerArea.innerHTML = '';
+    speakersList.forEach((speaker) => {
+      speakerArea.innerHTML += `
         <div class="speaker-card">
             <img id="picture" src='${speaker.pic}' alt='${speaker.name}'>
             <div id="content">
@@ -55,18 +54,18 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
         </div>
         `;
-      });
-      seeMoreButton.classList.add('active');
-      seeMoreButton.innerHTML = `
+    });
+    seeMoreButton.classList.add('active');
+    seeMoreButton.innerHTML = `
         SHOW LESS<img src="images/angle-up.svg">
-        `; 
-      }
-  
-      function hideSpeakers () {
-      speakerArea.innerHTML = '';
-      for(i=0; i<2; i++) {
-          speaker = speakersList[i];
-          speakerArea.innerHTML += `
+        `;
+  }
+
+  function hideSpeakers() {
+    speakerArea.innerHTML = '';
+    for (let i = 0; i < 2;) {
+      const speaker = speakersList[i];
+      speakerArea.innerHTML += `
           <div class="speaker-card">
               <img id="picture" src='${speaker.pic}' alt='${speaker.name}'>
               <div id="content">
@@ -77,20 +76,18 @@ document.addEventListener('DOMContentLoaded', () => {
               </div>
           </div>
           `;
-      }
-      seeMoreButton.classList.remove('active');
-      seeMoreButton.innerHTML = `
+    }
+    seeMoreButton.classList.remove('active');
+    seeMoreButton.innerHTML = `
           MORE<img src="images/angle-down.svg">    
       `;
   }
-  
-      seeMoreButton.addEventListener('click', () => {
-      if(seeMoreButton.classList.contains('active')) {
-          console.log('hello');
-          seeMoreButton.addEventListener('click', hideSpeakers);
-      }
-      else {
-          seeMoreButton.addEventListener('click', addSpeakers);
-      }
-      })
+
+  seeMoreButton.addEventListener('click', () => {
+    if (seeMoreButton.classList.contains('active')) {
+      seeMoreButton.addEventListener('click', hideSpeakers);
+    } else {
+      seeMoreButton.addEventListener('click', addSpeakers);
+    }
   });
+});
